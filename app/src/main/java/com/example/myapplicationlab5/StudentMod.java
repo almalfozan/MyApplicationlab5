8a6a4412ed48546cda8d7f2d767fc13c93a4e551
almalfozan <almalfozan@gmail.com>
2023-04-13 22:59:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationlab5/StudentMod.java
@@ -0,0 +1,51 @@
+package com.example.myapplicationlab5;
+
+public class StudentMod {
+    private int id;
+
+    private int age;
+
+    private String name;
+
+    public StudentMod(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public StudentMod() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentMod{" +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
